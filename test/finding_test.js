eff90baade56d7538ae3a6c13c2e236374500e58
rvisharma <rvisharma91@gmail.com>
2017-02-05 21:13:52 +0530

    ch 11 completed

--- test/finding_test.js
+++ test/finding_test.js
@@ -3,26 +3,38 @@ const assert = require('assert');
 const MarioChar = require('../models/marioChar');
 
 describe('Finding records', () => {
+    var char;
+    const callDone = (done) => () => done();
 
-    const callDone = (done) => () => done();    
-    
     beforeEach((done) => {
-        
-        new MarioChar({
+
+        char = new MarioChar({
             name: 'Mario'
-        })
-            .save()
-            .then(callDone(done));
+        });
+        char.save().then(callDone(done));
     })
 
     it('Find one record from the database', (done) => {
         MarioChar.findOne({
-            name: 'Mario'
-        })
+                name: 'Mario'
+            })
             .then((result) => {
                 assert(result !== undefined);
                 assert(result.name === 'Mario');
             })
             .then(callDone(done));
     })
+
+    it('Find one record by ID from the database', (done) => {
+        console.log(char._id.toString());
+        MarioChar.findOne({
+                _id: char._id
+            })
+            .then((result) => {
+                assert(result._id.toString() === char._id.toString());
+                assert(result.name === 'Mario');
+            })
+            .then(callDone(done));
+    })
+
 })
