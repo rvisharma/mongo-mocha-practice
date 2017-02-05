edb044e82178b1b020bb69edf3487c5c28bda28e
rvisharma <rvisharma91@gmail.com>
2017-02-05 22:22:07 +0530

    ch 13 completed - Updating Records

--- test/updating_test.js
+++ test/updating_test.js
@@ -1,17 +1,22 @@
 require('mocha');
 const assert = require('assert');
 const MarioChar = require('../models/marioChar');
-const {
-    callDone
-} = require('./util');
+const { callDone } = require('./util');
 
 describe('Updating Records', () => {
     let char;
 
     beforeEach((done) => {
-        char = new MarioChar({
-            name: 'Mario'
-        });
+        char = new MarioChar({ name: 'Mario' });
         char.save().then(callDone(done));
+    })
+
+    it('Update one record in the database', () => {
+        MarioChar.findOneAndUpdate({ name: 'Mario' }, { name: 'Luigi' })
+            .then(() => {
+                MarioChar.findOne({ _id: char._id }).then((result) => {
+                    assert(result.name === 'Luigi');
+                })
+            })
     })
 })
